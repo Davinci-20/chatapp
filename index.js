6f4197bfda1bf2eb1560e73032e83b01b073167d
kasungset <85478889+Davinci-20@users.noreply.github.com>
2021-12-13 14:14:43 +0630

    Chat App with Socket.io

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ let app = express();
 let server = app.listen(3000, () => {
   console.log("project is running on localhost 3000");
 });
+
 /**route setup */
 app.get("/", (res, req) => {
   req.sendFile(__dirname + "/public/index.html");
 });
+
 /**socket setup */
 let io = socket(server);
 io.on("connection", (socket) => {
-  console.log("Socket connection connected" + socket.id);
-}); /*Client to get browser connection */
+  /*Client to get browser connection */
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data); /**return to show datas to client */
+  });
+  socket.on("typing", (name) => {
+    socket.broadcast.emit("typing", name);
+  });
+});
+
+/** */
